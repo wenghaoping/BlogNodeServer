de7d207896b1726ba5363369ac6d9e81d8b79871
wenghaoping <wenghaoping@sina.com>
2017-09-30 17:38:03 +0800

    node博客后台

---
+++ settings.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Danny on 2015/9/25 11:31.
+ */
+module.exports = {
+    "dburl" : "mongodb://root:0p-0p-0p-@120.77.37.188:27017/admin",
+    "user" : "root",
+    "password" : "0p-0p-0p-",
+}
